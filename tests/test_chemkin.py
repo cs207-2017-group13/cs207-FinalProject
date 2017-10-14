096780abaa6a89f3427f07b9219aeb6e618064b3
Shiyun Qiu <shiyunqiu@Shiyuns-MacBook-Pro.local>
2017-10-14 19:00:36 -0400

    test_chemkin updated

--- tests/test_chemkin.py
+++ tests/test_chemkin.py
@@ -1,18 +1,18 @@
 import chemkin
 
 def test_len():
-	reader = XMLReader("test_data1.xml")
+	reader = chemkin.XMLReader("test_data1.xml")
 	reaction_system = reader.get_reaction_systems()
 	assert len(reaction_system[0]) == 5
 
 def test_progress_rate():
-	reader = XMLReader("test_data1.xml")
+	reader = chemkin.XMLReader("test_data1.xml")
 	reaction_system = reader.get_reaction_systems()
 	result = [2.5589111307566812, 1110.2037988957545, 0.0070156249238785568]
 	assert reaction_system[0].calculate_progress_rate([3., 1., 2., 3., 1.], 425) == result
 
 def test_neg_concentration():
-	reader = XMLReader("test_data1.xml")
+	reader = chemkin.XMLReader("test_data1.xml")
 	reaction_system = reader.get_reaction_systems()
 	try:
 		reaction_system[0].calculate_progress_rate([3., -1., 2., -3., 1.], 425)
@@ -20,7 +20,7 @@ def test_neg_concentration():
 		assert(type(err) == ValueError)
 
 def test_reaction_rate():
-	reader = XMLReader("test_data1.xml")
+	reader = chemkin.XMLReader("test_data1.xml")
 	reaction_system = reader.get_reaction_systems()
 	result = [ 1107.64488776, -1107.64488776,  1112.77674128, -1110.21081452, -2.56592676]
 	assert reaction_system[0].calculate_reaction_rate([3., 1., 2., 3., 1.], 425) == result
