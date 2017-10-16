069b9d47a72be8a4d21bd8ddeecc92a67485a1e3
Shiyun Qiu <shiyunqiu@Shiyuns-MacBook-Pro.local>
2017-10-15 20:06:03 -0400

    Indentation fixed

--- tests/test_chemkin.py
+++ tests/test_chemkin.py
@@ -106,7 +106,8 @@ def test_neg_concentration():
 		assert(type(err) == ValueError)
 
 def test_reaction_rate():
-	reader = chemkin.XMLReader("tests/test_data1.xml")
-	reaction_system = reader.get_reaction_systems()
-    result = [ 1107.64488776, -1107.64488776,  1112.77674128, -1110.21081452, -2.56592676]
-	assert all(reaction_system[0].calculate_reaction_rate([3., 1., 2., 3., 1.], 425) == result)
+    reader = chemkin.XMLReader("tests/test_data1.xml")
+    reaction_system = reader.get_reaction_systems()
+    result = [ 1107.64488776, -1107.64488776,  1112.77674128, -1110.21081452, -2.56592676 ]
+    assert all(reaction_system[0].calculate_reaction_rate([3., 1., 2., 3., 1.], 425) == result)
+
