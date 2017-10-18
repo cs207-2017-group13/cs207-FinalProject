d33e0b315beffa0f7f9670774cf1daee4041445d
Victor Zhao <vzhao23@gmail.com>
2017-10-18 11:26:23 -0400

    setup.py for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="chemical_kinetics",
+    version="0.0.1",
+    author=["Divyam Misra", "Shiyun Qiu", "Victor Zhao"],
+    description=("Chemical kinetics library"),
+    license="BSD",
+    package_dir={
+        '': "src"
+    }
+)
