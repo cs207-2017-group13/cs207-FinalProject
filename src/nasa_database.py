0f4ecfedd45b4b5b523b09fe10859bbc2d2cbbdd
Shiyun Qiu <shiyunqiu@Shiyuns-MacBook-Pro.local>
2017-11-12 12:00:03 -0500

    Removed duplicated database and rename file name

--- src/nasa_database.py
+++ src/nasa_database.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python3
+
 import sqlite3
 import re
 
+
 # create database
-db = sqlite3.connect('NASAPolyCoeff.sqlite')
+db = sqlite3.connect('NASA_polynomial_coefficients.sqlite')
 cursor = db.cursor()
-cursor.execute("DROP TABLE IF EXISTS PolyCoeff")
-cursor.execute("PRAGMA foreign_keys=1")
-cursor.execute('''CREATE TABLE PolyCoeff (
+cursor.execute("DROP TABLE IF EXISTS coefficients")
+cursor.execute('''CREATE TABLE coefficients (
                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-               specie TEXT NOT NULL, 
-               coeff TEXT NOT NULL, 
-               temp TEXT NOT NULL)''')
+               species TEXT NOT NULL,
+               low_temp DECIMAL(10,2) NOT NULL,
+               mid_temp DECIMAL(10,2) NOT NULL,
+               high_temp DECIMAL(10,2) NOT NULL,
+               low_coeffs TEXT NOT NULL,
+               high_coeffs TEXT NOT NULL
+               )''')
 db.commit()
 
 # read from the text file
@@ -23,26 +29,31 @@ thermo = thermo[5:-5]
 thermo_list = []
 
 for i, item in enumerate(thermo):
-    if i%4 == 0:
+    if i % 4 == 0:
+        low_t = float(item[45:55])
+        high_t = float(item[55:65])
+        mid_t = float(item[65:73])
         items = item.split()
-        specie = items[0]
-    elif i%4 == 1:
+        species = items[0]
+    elif i % 4 == 1:
         items = re.findall('[-]?[0-9]+\.[0-9]+[E][+-][0-9]+', item)
         high_temp = items
-    elif i%4 == 2:
+    elif i % 4 == 2:
         items = re.findall('[-]?[0-9]+\.[0-9]+[E][+-][0-9]+', item)
         high_temp += items[:2]
         low_temp = items[2:]
     else:
         items = re.findall('[-]?[0-9]+\.[0-9]+[E][+-][0-9]+', item)
         low_temp += items
-        thermo_list.append([specie, ', '.join(low_temp), 'low'])
-        thermo_list.append([specie, ', '.join(high_temp), 'high'])
+        thermo_list.append(
+            [species, low_t, mid_t, high_t,
+             ' '.join(low_temp), ' '.join(high_temp)])
 
 # insert into the database
-for i, item in enumerate(thermo_list):
-    cursor.execute('''INSERT INTO PolyCoeff (specie, coeff, temp) 
-                               VALUES (?, ?, ?)''', item)
+cursor.executemany('''
+    INSERT INTO coefficients
+    (species, low_temp, mid_temp, high_temp, low_coeffs, high_coeffs)
+    VALUES (?, ?, ?, ?, ?, ?)''', thermo_list)
 
 db.commit()
 db.close()
