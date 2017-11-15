f2f559c14d7323ead32b3b0c956e74ce41d592a0
Victor Zhao <vzhao23@gmail.com>
2017-11-15 14:55:13 -0500

    test build

--- src/thermodynamics.py
+++ src/thermodynamics.py
@@ -3,7 +3,7 @@ thermodynamics.py
 
 victor: My feeling is that chemkin.py is getting to be too long.
 
-We should be able to separate out some things into this file.
+We should be able to separate out some things into this file.d
 
 """
 import collections
