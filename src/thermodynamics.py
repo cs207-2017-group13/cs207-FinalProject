2ed777fb024f435ce6392dec125467eff3085110
Victor Zhao <vzhao23@gmail.com>
2017-11-14 22:10:19 -0500

    thermmodynamics suggestion file

---
+++ src/thermodynamics.py
@@ -0,0 +1,15 @@
+"""
+thermodynamics.py
+
+victor: My feeling is that chemkin.py is getting to be too long.
+
+We should be able to separate out some things into this file.
+
+"""
+class Thermochem():
+    """
+    As described, the reverse reaction rate are given.
+
+    """
+
+
