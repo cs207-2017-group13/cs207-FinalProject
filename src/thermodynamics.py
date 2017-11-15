0e65a838a69723c0a5b92c353c719f02f4e9f9fc
Victor Zhao <vzhao23@gmail.com>
2017-11-15 15:06:10 -0500

    sdf

--- src/thermodynamics.py
+++ src/thermodynamics.py
@@ -3,7 +3,7 @@ thermodynamics.py
 
 victor: My feeling is that chemkin.py is getting to be too long.
 
-We should be able to separate out some things into this file.d
+We should be able to separate out some things into this file.
 
 """
 import collections
