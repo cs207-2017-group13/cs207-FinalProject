77edf0f6e70c5fb5c5a4e751a2c72fca0b11dbae
Victor Zhao <vzhao23@gmail.com>
2017-11-19 11:38:35 -0500

    fixed setup.py finally

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="chemical_kinetics",
+    name="chemical-kinetics",
     version="0.0.1",
     author=["Divyam Misra", "Shiyun Qiu", "Victor Zhao"],
     description=("Chemical kinetics library"),
     license="BSD",
-    packages=find_packages()
+    packages=find_packages('src'),
+    package_dir={"": "src"},
+    package_data={'chemkin': ["*.sqlite"]}
 )
