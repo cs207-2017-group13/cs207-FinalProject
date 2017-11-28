f5199c6cd57d7cf502f8bf709a4ec2ccfac08cc7
Shiyun Qiu <shiyunqiu@Shiyuns-MacBook-Pro.local>
2017-11-28 12:56:56 -0500

    ode_solver minor changes

--- src/chemkin/ode_solver.py
+++ src/chemkin/ode_solver.py
@@ -10,7 +10,6 @@ class ODE_solver:
         self.dt = dt
         self.t = [0.]
         self.y = [y0]
-        self.h = dt
 
     def backward_euler(self, epsilon = 1e-06):
         nsteps = t_final//dt
@@ -50,59 +49,58 @@ class ODE_solver:
         return "Success"
 
     def rk45(self, epsilon = 1e-06):
-        h = self.dt
         y = [self.y0]
-        while (self.t[-1]+h) <= self.t_final:
-            k1 = h*self.diff_function(self.t[-1], y[-1])
-            k2 = h*self.diff_function(self.t[-1]+h/4, y[-1]+k1/2)
-            k3 = h*self.diff_function(self.t[-1]+3*h/8, y[-1]+3*k1/32+9*k2/32)
-            k4 = h*self.diff_function(self.t[-1]+12*h/13, 
+        while (self.t[-1]+self.dt) <= self.t_final:
+            k1 = self.dt*self.diff_function(self.t[-1], y[-1])
+            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, y[-1]+k1/2)
+            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, y[-1]+3*k1/32+9*k2/32)
+            k4 = self.dt*self.diff_function(self.t[-1]+12*self.dt/13, 
                 y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
-            k5 = h*self.diff_function(self.t[-1]+h, 
+            k5 = self.dt*self.diff_function(self.t[-1]+self.dt, 
                 y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
-            k6 = h*self.diff_function(self.t[-1]+h/2, 
+            k6 = self.dt*self.diff_function(self.t[-1]+self.dt/2, 
                 y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
             w1 = y[-1] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
             w2 = y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
-            q = 0.84 * (epsilon*h/abs(w2-w1))**0.25
+            q = 0.84 * (epsilon*self.dt/abs(w2-w1))**0.25
             if q>=1:
                 y.append(w2)
-                self.t.append(self.t[-1]+h)
+                self.t.append(self.t[-1]+self.dt)
             if q <= 0.1:
-                h = 0.1*h
+                self.dt = 0.1*self.dt
             elif q>= 4:
-                h = 4*h
+                self.dt = 4*self.dt
             else:
-                h = q*h
+                self.dt = q*self.dt
         y = np.array(y)
         return y.transpose()
 
     def rk45_step(self, epsilon = 1e-06):
         while (self.t[-1]+self.h) <= self.t_final:
-            k1 = self.h*self.diff_function(self.t[-1], self.y[-1])
-            k2 = self.h*self.diff_function(self.t[-1]+self.h/4, self.y[-1]+k1/2)
-            k3 = self.h*self.diff_function(self.t[-1]+3*self.h/8, self.y[-1]+3*k1/32+9*k2/32)
-            k4 = self.h*self.diff_function(self.t[-1]+12*self.h/13, 
+            k1 = self.dt*self.diff_function(self.t[-1], self.y[-1])
+            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2)
+            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32)
+            k4 = self.dt*self.diff_function(self.t[-1]+12*self.dt/13, 
                 self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
-            k5 = self.h*self.diff_function(self.t[-1]+self.h, 
+            k5 = self.dt*self.diff_function(self.t[-1]+self.dt, 
                 self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
-            k6 = self.h*self.diff_function(self.t[-1]+self.h/2, 
+            k6 = self.dt*self.diff_function(self.t[-1]+self.dt/2, 
                 self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
             w1 = self.y[-1] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
             w2 = self.y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
-            q = 0.84 * (epsilon*self.h/abs(w2-w1))**0.25
+            q = 0.84 * (epsilon*self.dt/abs(w2-w1))**0.25
             if q>=1:
                 self.y.append(w2)
-                self.t.append(self.t[-1]+self.h)
+                self.t.append(self.t[-1]+self.dt)
                 if q>= 4:
-                    self.h = 4*self.h
+                    self.dt = 4*self.dt
                 else:
-                    self.h = q*self.h
+                    self.dt = q*self.dt
                 return "Success"
             if q <= 0.1:
-                self.h = 0.1*self.h
+                self.dt = 0.1*self.dt
             else:
-                self.h = q*self.h
+                self.dt = q*self.dt
             
 
 
