7d90a2fe1f61f2df26ef4dd647d1e26613f3a226
Shiyun Qiu <shiyunqiu@Shiyuns-MacBook-Pro.local>
2017-11-28 17:39:27 -0500

    ODE Solver Version 2

--- src/chemkin/ode_solver.py
+++ src/chemkin/ode_solver.py
@@ -3,36 +3,33 @@ import matplotlib.pyplot as plt
 import matplotlib 
 
 class ODE_solver:
-    def __init__(self, func, y0, t_final, dt):
+    def __init__(self, func, y0, t_span, dt):
         self.diff_function = func
-        self.y0 = y0
-        self.t_final = t_final
+        self.t_span = t_span
         self.dt = dt
-        self.t = [0.]
+        self.t = [self.t_span[0]]
         self.y = [y0]
 
     def backward_euler(self, epsilon = 1e-06):
-        nsteps = t_final//dt
-        y = np.zeros(nsteps+1, len(self.y0))
-        y[0] = self.y0
+        nsteps = t_span[-1]//dt
         for i in range(nsteps):
-            y_prev = y[i]
-            y_curr = y[i] + self.dt*self.diff_function((i+1)*self.dt, y_prev)
+            y_prev = self.y[-1]
+            y_curr = self.y[-1] + self.dt*self.diff_function((i+1)*self.dt, y_prev)
             j = 0
             # fixed point iteration
             while abs(y_curr-y_prev) > epsilon:
                 y_prev = y_curr
-                y_curr = y[i] + self.dt*self.diff_function((i+1)*self.dt, y_prev)
+                y_curr = self.y[-1] + self.dt*self.diff_function((i+1)*self.dt, y_prev)
                 j += 1
                 if j>10000:
                     raise RuntimeError("The sequence does not converge.")
             # assign the approximation to result
-            y[i+1] = y_curr
+            self.y.append(y_curr)
             self.t.append((i+1)*self.dt)
-        return y.transpose()
+        return self.t, self.y
 
     def backward_euler_step(self, epsilon = 1e-06):
-        if self.t[-1] + self.dt <= self.t_final:
+        if self.t[-1] + self.dt <= self.t_span[-1]:
             y_prev = self.y[-1]
             y_curr = self.y[-1] + self.dt*self.diff_function(self.t[-1]+self.dt, y_prev)
             j = 0
@@ -49,22 +46,21 @@ class ODE_solver:
         return "Success"
 
     def rk45(self, epsilon = 1e-06):
-        y = [self.y0]
-        while (self.t[-1]+self.dt) <= self.t_final:
-            k1 = self.dt*self.diff_function(self.t[-1], y[-1])
-            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, y[-1]+k1/2)
-            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, y[-1]+3*k1/32+9*k2/32)
+        while (self.t[-1]+self.dt) <= self.t_span[-1]:
+            k1 = self.dt*self.diff_function(self.t[-1], self.y[-1])
+            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2)
+            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32)
             k4 = self.dt*self.diff_function(self.t[-1]+12*self.dt/13, 
-                y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
+                self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
             k5 = self.dt*self.diff_function(self.t[-1]+self.dt, 
-                y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
+                self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
             k6 = self.dt*self.diff_function(self.t[-1]+self.dt/2, 
-                y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
-            w1 = y[-1] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
-            w2 = y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
+                self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
+            w1 = self.y[-1] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
+            w2 = self.y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
             q = 0.84 * (epsilon*self.dt/abs(w2-w1))**0.25
             if q>=1:
-                y.append(w2)
+                self.y.append(w2)
                 self.t.append(self.t[-1]+self.dt)
             if q <= 0.1:
                 self.dt = 0.1*self.dt
@@ -72,11 +68,10 @@ class ODE_solver:
                 self.dt = 4*self.dt
             else:
                 self.dt = q*self.dt
-        y = np.array(y)
-        return y.transpose()
+        return self.t, self.y
 
     def rk45_step(self, epsilon = 1e-06):
-        while (self.t[-1]+self.h) <= self.t_final:
+        while (self.t[-1]+self.dt) <= self.t_span[-1]:
             k1 = self.dt*self.diff_function(self.t[-1], self.y[-1])
             k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2)
             k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32)
--- src/chemkin/simulator.py
+++ src/chemkin/simulator.py
@@ -1,3 +1,4 @@
+import chemkin.ode_solver as ode_solver
 class ReactionSimulator():
     def plot(self, t, y, species):
         # plot y's 
@@ -15,23 +16,47 @@ class StochasticSimulator(ReactionSimulator):
 
 
 class DeterministicSimulator(ReactionSimulator):
-    def __init__(self, reaction_system, initial_concentrations, temperature):
+    def __init__(self, reaction_system, initial_concentrations, temperature, t_span, dt=1):
         self.reaction_system = reaction_system
         if temperature <=0:
             raise ValueError("Temperature must be positive.")
         self.temperature = temperature
-        # assert concentrations shape is number of species in reaction system
-        self.time = [0.]
+        if len(initial_concentrations) != len(self.reaction_system.species):
+            raise ValueError("Invalid initial concentration.")
+        self.time = [t_span[0]]
         self.concentrations = [initial_concentrations]
+        self.t_span = t_span
+        self.dt = dt
+        self.ode_solver = ode_solver.ODE_solver(
+            self.diff_func, initial_concentrations, t_span, self.dt)
 
-    def simulate(self, t_final, method='rk45'):
+    def simulate(self, method='rk45', epsilon = 1e-06):
+        choices = ['backward_euler','rk45']
+        if method not in choices:
+            raise ValueError("Wrong method.")
+        if method == 'rk45':
+            self.time, self.concentrations = self.ode_solver.rk45(epsilon)
+        else:
+            self.time, self.concentrations = self.ode_solver.backward_euler(epsilon)
+
+    def simulate_step(self, method='rk45', epsilon = 1e-06):
         """Calculate concentrations between last time and `t_final`.
-
+        Neccessary? refer to scipy.integrate.solve_ivp
         """
         # initialize solver
-        while t < t_final:
-            self.step()
-        # save data
+        if self.time[-1] < self.t_span[-1]:
+            choices = ['backward_euler','rk45']
+            if method not in choices:
+                raise ValueError("Wrong method.")
+            if method == 'rk45':
+                message = self.ode_solver.rk45_step(epsilon)
+                self.dt, self.time, self.concentrations = self.ode_solver.dt, self.ode_solver.t, self.ode_solver.y
+            else:
+                message = self.ode_solver.backward_euler_step(epsilon)
+                self.dt, self.time, self.concentrations = self.ode_solver.dt, self.ode_solver.t, self.ode_solver.y
+        else:
+            raise IndexError("Time exceeds time span.")
+        return message
 
     def diff_func(self, t, y):
         self.reaction_system.calculate_reaction_rate(y, self.temperature)
