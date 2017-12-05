f5e038f06aa9593ce12490187643f879dc5538f8
Shiyun Qiu <shiyunqiu@g.harvard.edu>
2017-12-05 15:55:40 -0500

    Just another test, error expected

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -22,10 +22,10 @@ def test_neg_temp():
 	except ValueError as err:
 		assert (type(err) == ValueError)
 
-def test_sim_backward_euler():
-	concs = [1., 2., 1., 3., 1.]
-	reader = chemkin.XMLReader("tests/rxns.xml")
-	reaction_system = reader.get_reaction_systems()[0]
-	det_sim = simulator.DeterministicSimulator(reaction_system,
-		concs, 800, [0, 2], dt=1)
-	assert det_sim.simulate('backward_euler') == 1
+# def test_sim_backward_euler():
+# 	concs = [1., 2., 1., 3., 1.]
+# 	reader = chemkin.XMLReader("tests/rxns.xml")
+# 	reaction_system = reader.get_reaction_systems()[0]
+# 	det_sim = simulator.DeterministicSimulator(reaction_system,
+# 		concs, 800, [0, 2], dt=1)
+# 	assert det_sim.simulate('backward_euler') == 1
