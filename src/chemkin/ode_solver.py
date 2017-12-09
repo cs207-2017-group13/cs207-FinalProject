76808ddbcf473e6c67d663b8fe3fbb07dede6cd9
Shiyun Qiu <shiyunqiu@g.harvard.edu>
2017-12-09 11:30:36 -0500

    add tests for ode solver test for robustness

--- src/chemkin/ode_solver.py
+++ src/chemkin/ode_solver.py
@@ -1,5 +1,6 @@
 from scipy.integrate import ode
 from numpy.linalg import norm
+import numpy as np
 
 class ODE_solver:
     """ODE Solver class
@@ -36,8 +37,21 @@ class ODE_solver:
         self.t_span = t_span
         self.dt = dt
         self.t = [self.t_span[0]]
-        self.y = [y0]
         self.neg_y_allowed =neg_y_allowed
+        if (not self.neg_y_allowed):
+            if (type(y0) is float) or (type(y0) is int):
+                if y0<0:
+                    self.y = [0.]
+                else:
+                    self.y = [y0]
+            else:
+                if (np.array(y0)<0).any():
+                    self.y = [[0. if i < 0 else i for i in y0]]
+                else:
+                    self.y = [y0]
+        else:
+            self.y = [y0]
+        
 
     def backward_euler(self, epsilon = 1e-06):
         """Solve the ODE using backward euler method.
@@ -96,39 +110,39 @@ class ODE_solver:
         if self.t[-1] + self.dt <= self.t_span[-1]:
             # fixed point iteration
             prev_y = self.y[-1]
-            curr_y = self.y[-1]+ self.dt*self.diff_function(self.t[-1]+self.dt, prev_y)
+            curr_y = self.y[-1]+ self.dt*np.array(self.diff_function(self.t[-1]+self.dt, prev_y))
             # count number of iterations to avoid looping endlessly
             j = 1
             while (norm(abs(curr_y-prev_y)) > epsilon) & (j<=1000):
                 j+=1
                 prev_y = curr_y
-                curr_y = self.y[-1]+ self.dt*self.diff_function(self.t[-1]+self.dt, prev_y)
+                curr_y = self.y[-1]+ self.dt*np.array(self.diff_function(self.t[-1]+self.dt, prev_y))
             if j > 1000:
                 return "Failure"
-            if type(curr_y) is float:
+            if (type(curr_y) is float) or (type(curr_y) is int):
                 if (not self.neg_y_allowed) and (curr_y < 0):
-                    curr_y = 0
+                    curr_y = 0.
             else:
                 if (not self.neg_y_allowed) and (curr_y < 0).any():
-                    curr_y = [0 if i <0 else i for i in curr_y]
+                    curr_y = [0. if i <0 else i for i in curr_y]
             self.y.append(curr_y)
             self.t.append(self.t[-1]+self.dt)
         elif self.t[-1] < self.t_span[-1]:
             prev_y = self.y[-1]
-            curr_y = self.y[-1]+ self.dt*self.diff_function(self.t[-1]+self.dt, prev_y)
+            curr_y = self.y[-1]+ self.dt*np.array(self.diff_function(self.t[-1]+self.dt, prev_y))
             j = 1
             while (norm(abs(curr_y-prev_y)) > epsilon) & (j<=1000):
                 j += 1
                 prev_y = curr_y
-                curr_y = self.y[-1] + self.dt*self.diff_function(self.t[-1]+self.dt, prev_y)
+                curr_y = self.y[-1] + self.dt*np.array(self.diff_function(self.t[-1]+self.dt, prev_y))
             if j > 1000:
                 return "Failure"
-            if type(curr_y) is float:
+            if (type(curr_y) is float) or (type(curr_y) is int):
                 if (not self.neg_y_allowed) and (curr_y < 0):
-                    curr_y = 0
+                    curr_y = 0.
             else:
                 if (not self.neg_y_allowed) and (curr_y < 0).any():
-                    curr_y = [0 if i <0 else i for i in curr_y]
+                    curr_y = [0. if i <0 else i for i in curr_y]
             self.y.append(curr_y)
             self.t.append(self.t_span[-1])
         return "Success"
@@ -194,24 +208,24 @@ class ODE_solver:
             if (self.dt < 1e-10) & (warning_print == False):
                 print("Warning: Step size too small.")
                 warning_print = True
-            k1 = self.dt*self.diff_function(self.t[-1], self.y[-1])
-            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2)
-            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32)
-            k4 = self.dt*self.diff_function(self.t[-1]+12*self.dt/13, 
-                self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
-            k5 = self.dt*self.diff_function(self.t[-1]+self.dt, 
-                self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
-            k6 = self.dt*self.diff_function(self.t[-1]+self.dt/2, 
-                self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
+            k1 = self.dt*np.array(self.diff_function(self.t[-1], self.y[-1]))
+            k2 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2))
+            k3 = self.dt*np.array(self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32))
+            k4 = self.dt*np.array(self.diff_function(self.t[-1]+12*self.dt/13, 
+                self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197))
+            k5 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt, 
+                self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104))
+            k6 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt/2, 
+                self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40))
             w1 = self.y[-1] + 25*k1/216 + 1408*k3/2565 + 2197*k4/4104 - k5/5
             w2 = self.y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
             if norm(abs(w2-w1))==0:
-                if type(w2) is float:
+                if (type(w2) is float) or (type(w2) is int):
                     if (not self.neg_y_allowed) and (w2 < 0):
-                        w2 = 0
+                        w2 = 0.
                 else:
                     if (not self.neg_y_allowed) and (w2 < 0).any():
-                        w2 = [0 if i <0 else i for i in w2]
+                        w2 = [0. if i <0 else i for i in w2]
                 self.y.append(w2)
                 self.t.append(self.t[-1]+self.dt)
                 self.dt = 4*self.dt
@@ -220,12 +234,12 @@ class ODE_solver:
             q = norm(0.84 * (epsilon*self.dt/abs(w2-w1))**0.25)
             # adjust step size
             if q>=1:
-                if type(w2) is float:
+                if (type(w2) is float) or (type(w2) is int):
                     if (not self.neg_y_allowed) and (w2 < 0):
-                        w2 = 0
+                        w2 = 0.
                 else:
                     if (not self.neg_y_allowed) and (w2 < 0).any():
-                        w2 = [0 if i <0 else i for i in w2]
+                        w2 = [0. if i <0 else i for i in w2]
                 self.y.append(w2)
                 self.t.append(self.t[-1]+self.dt)
                 if q>= 4:
@@ -240,22 +254,22 @@ class ODE_solver:
                 self.dt = q*self.dt
         if ((self.t[-1]+self.dt) > self.t_span[-1]) & (j<1000):
             self.dt = self.t_span[-1]-self.t[-1]
-            k1 = self.dt*self.diff_function(self.t[-1], self.y[-1])
-            k2 = self.dt*self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2)
-            k3 = self.dt*self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32)
-            k4 = self.dt*self.diff_function(self.t[-1]+12*self.dt/13, 
-                self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197)
-            k5 = self.dt*self.diff_function(self.t[-1]+self.dt, 
-                self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104)
-            k6 = self.dt*self.diff_function(self.t[-1]+self.dt/2, 
-                self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40)
+            k1 = self.dt*np.array(self.diff_function(self.t[-1], self.y[-1]))
+            k2 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt/4, self.y[-1]+k1/2))
+            k3 = self.dt*np.array(self.diff_function(self.t[-1]+3*self.dt/8, self.y[-1]+3*k1/32+9*k2/32))
+            k4 = self.dt*np.array(self.diff_function(self.t[-1]+12*self.dt/13, 
+                self.y[-1]+1932*k1/2197-7200*k2/2197+7296*k3/2197))
+            k5 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt, 
+                self.y[-1]+439*k1/216-8*k2+3680*k3/513-845*k4/4104))
+            k6 = self.dt*np.array(self.diff_function(self.t[-1]+self.dt/2, 
+                self.y[-1]-8*k1/27+2*k2-3544*k3/2565+1859*k4*4104-11*k5/40))
             w2 = self.y[-1] + 16*k1/135 + 6656*k3/12825 + 28561*k4/56430 -9*k5/50 + 2*k6/55
-            if type(w2) is float:
+            if (type(w2) is float) or (type(w2) is int):
                 if (not self.neg_y_allowed) and (w2 < 0):
-                    w2 = 0
+                    w2 = 0.
             else:
                 if (not self.neg_y_allowed) and (w2 < 0).any():
-                    w2 = [0 if i <0 else i for i in w2]
+                    w2 = [0. if i <0 else i for i in w2]
             self.t.append(self.t_span[-1])
             self.y.append(w2)
             message = "Success"
@@ -292,12 +306,12 @@ class ODE_solver:
                 y_val=r.y[0]
             else:
                 y_val = r.y
-            if type(y_val) is float:
+            if (type(y_val) is float) or (type(y_val) is int):
                 if (not self.neg_y_allowed) and (y_val < 0):
-                    y_val = 0
+                    y_val = 0.
             else:
                 if (not self.neg_y_allowed) and (y_val < 0).any():
-                    y_val = [0 if i <0 else i for i in y_val]
+                    y_val = [0. if i <0 else i for i in y_val]
             self.y.append(y_val)
             self.t.append(r.t)
         return self.t, self.y
@@ -305,3 +319,4 @@ class ODE_solver:
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
