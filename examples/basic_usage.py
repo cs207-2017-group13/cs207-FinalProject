b5ec425cd9ade16a55375fcc760cd95abff559e6
Shiyun Qiu <shiyunqiu@g.harvard.edu>
2017-12-10 12:22:43 -0500

    Basic usage agree

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -7,21 +7,23 @@ reader = chemkin.XMLReader("tests/rxns.xml")
 reaction_system = reader.get_reaction_systems()[0]
 react_rate = reaction_system.calculate_reaction_rate(concs, 800)
 
-# deterministic simulation
-det_sim = reaction_system.setup_reaction_simulator('deterministic', concs, 800, [0, 10], dt=0.01)
-t, y = det_sim.simulate()
-det_sim.plot_simulation()
-
-# stochastic simulation
-abundances = [10., 10., 10., 10., 10.]
-stoch_sim = reaction_system.setup_reaction_simulator('stochastic', abundances, 800, [0, 100], 1e-15)
-stoch_sim.simulate()
-stoch_sim.plot_simulation()
-
 # reversible reactions
-concs = [1, 2, 3, 1, 2, 3, 1, 2]
+abundances = np.array([10., 10., 10., 10., 10.])
+concs = abundances/6.02e23/1e-15
 reader = chemkin.XMLReader("tests/rxns_reversible.xml")
 reaction_system = reader.get_reaction_systems()[0]
 react_rate = reaction_system.calculate_reaction_rate(concs, 800)
+
+# deterministic simulation
+det_sim = reaction_system.setup_reaction_simulator('deterministic', concs, 400, [0, 1e-4], dt=1e-7)
+det_sim.simulate(epsilon=1e-9)
+
+# stochastic simulation
+stoch_sim = reaction_system.setup_reaction_simulator('stochastic', abundances, 400, [0, 1e-4], 1e-15)
+stoch_sim.simulate()
+
+# compare the plots of deterministic simulation and stochastic simulation
+det_sim.plot_simulation()
+stoch_sim.plot_simulation()
 
 
